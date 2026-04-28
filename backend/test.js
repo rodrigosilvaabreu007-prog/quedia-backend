deaff4ddb1dde0617ba960dd9cdb9c4dd16c3bd6
Rodrigo Silva Abreu <rodrigo.silva.abreu554466@gmail.com>
2026-04-28 12:17:42 -0300

    Remove all test files from root, frontend and backend directories

--- backend/test.js
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-http.get('http://localhost:3001/', res => {
-  let data = '';
-  res.on('data', d => data += d);
-  res.on('end', () => console.log('Resposta:', data));
-}).on('error', err => console.log('Erro:', err.message));
--- backend/testes.sql
+++
@@ -1,4 +0,0 @@
--- Remover eventos de teste
-DELETE FROM eventos WHERE nome LIKE 'Teste%';
--- Remover usuários de teste
-DELETE FROM usuarios WHERE email LIKE 'teste%';
