7f4602e0d09caaa4a4ca744c53be1f2924d10747
Rodrigo Silva Abreu <rodrigo.silva.abreu554466@gmail.com>
2026-04-30 13:35:35 -0300

    Fix Firebase Functions backend packaging

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,11 @@
 const functions = require('firebase-functions');
-const app = require('./src/server');
+const admin = require('firebase-admin');
+
+// Inicializar Firebase Admin SDK para Functions
+if (!admin.apps.length) {
+  admin.initializeApp();
+}
+
+const app = require('eventhub-backend/src/server');
 
 exports.api = functions.https.onRequest(app);
